d1ee9f3f872cc46e19d18a712ce8968886d24a01
Yash Kumar <ykumar6@gmail.com>
2015-04-24 17:07:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, blah blah\n');
+  res.end('Hello, 23r223r23r blah\n');
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
